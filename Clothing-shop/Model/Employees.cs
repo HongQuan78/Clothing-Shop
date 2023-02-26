8c33bfbc82323db36c87355c03557888b52c706b
Quanvo313 <quanvhce160078@fpt.edu.vn>
2023-02-26 18:11:50 +0700

    add model

---
+++ Clothing-shop/Model/Employees.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothing_shop.Model
+{
+    public class Employees
+    {
+
+        public String EmployeeID { get; set; }
+        public String EmployeeName { get; set; }
+        public String EmployeeRole { get; set; }
+        public String EmployeeUsername { get; set; }
+        public String EmployeePassword { get; set; }
+        public String EmployeePhone { get; set; }
+        public String EmployeeAddress { get; set; }
+        public Employees() { }
+        public Employees(string employeeID, string employeeName, string employeeRole, string employeeUsername, string employeePassword, string employeePhone, string employeeAddress)
+        {
+            EmployeeID = employeeID;
+            EmployeeName = employeeName;
+            EmployeeRole = employeeRole;
+            EmployeeUsername = employeeUsername;
+            EmployeePassword = employeePassword;
+            EmployeePhone = employeePhone;
+            EmployeeAddress = employeeAddress;
+        }
+    }
+}
